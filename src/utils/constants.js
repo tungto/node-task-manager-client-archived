9e4dea3bd5f36f7e110f35729d4ed8712ab846f9
tungto <toxtung@gmail.com>
2021-11-09 03:01:03 +0700

    change back-end url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-export const apiUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:8000' : 'https://ted-task-manager.herokuapp.com/';
+export const apiUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:8000' : 'https://ted-task-manager.herokuapp.com';
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'todo-mern';
